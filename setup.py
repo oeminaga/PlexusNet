47fd5adfde7b6c8f3b3a23f3dd9c7a88ce7a74c5
Okyaz Eminaga <okyaz.eminaga@stanford.edu>
2020-06-06 21:51:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ description='PlexusNet for medical image analysis',
 url='https://github.com/oeminaga/PlexusNet.git',
 author='Okyaz Eminaga',
 author_email='info@deepmedicine.ai',
+install_requires=["keras>=2.3.1"],
 license='Only for research purposes, GPL. Please cite when you use, replicate or modfiy the package.',
 packages=find_packages(),
 zip_safe=False)
