3b09e782b2283e96de96b2a658496cae3042fcd4
Okyaz Eminaga <okyaz.eminaga@stanford.edu>
2020-06-06 21:13:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 setup(name='PlexusNet',
 version='0.1',
 description='PlexusNet for medical image analysis',
-url='#https://github.com/oeminaga/PlexusNet.git',
+url='https://github.com/oeminaga/PlexusNet.git',
 author='Okyaz Eminaga',
 author_email='info@deepmedicine.ai',
 license='Only for research purposes, GPL. Please cite when you use, replicate or modfiy the package.',
-packages=['PlexusNet'],
+packages=['plexusnet'],
 zip_safe=False)
