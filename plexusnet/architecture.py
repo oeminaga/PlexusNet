68d0f1c8d5d1c93483a9edf21ff2cb1aab8f155d
Okyaz Eminaga <okyaz.eminaga@stanford.edu>
2020-12-09 00:32:55 -0800

    Update architecture.py

--- plexusnet/architecture.py
+++ plexusnet/architecture.py
@@ -25,7 +25,7 @@ def LoadModel(filename, custom_objects={},optimizer= optimizers.Adam(), loss="ca
     return model_
     
 class PlexusNet():
-    def __init__(self, input_shape=(512,512), initial_filter=2, length=2, depth=7, junction=3, n_class=2, number_input_channel=3, compression_rate=0.5,final_activation="softmax", random_junctions=True, run_all_BN=True ,type_of_block="inception", run_normalization=True, run_rescale=True, filter_num_for_first_convlayer=32, kernel_size_for_first_convlayer=(5,5),stride_for_first_convlayer=2,activation_for_first_convlayer="relu", add_crop_layer=False, crop_boundary=((5,5),(5,5)), get_last_conv=False, normalize_by_factor=1.0/255.0, apply_RandomFourierFeatures=False,MIL_mode=False, MIL_bag_size=8, GlobalPooling="max"):
+    def __init__(self, input_shape=(512,512), initial_filter=2, length=2, depth=7, junction=3, n_class=2, number_input_channel=3, compression_rate=0.5,final_activation="softmax", random_junctions=True, run_all_BN=True ,type_of_block="inception", run_normalization=True, run_rescale=True, filter_num_for_first_convlayer=32, kernel_size_for_first_convlayer=(5,5),stride_for_first_convlayer=2,activation_for_first_convlayer="relu", add_crop_layer=False, crop_boundary=((5,5),(5,5)), get_last_conv=False, normalize_by_factor=1.0/255.0, apply_RandomFourierFeatures=False,MIL_mode=False, MIL_bag_size=8, MIL_useGated=False, GlobalPooling="max"):
         """
         Architecture hyperparameter are:
         initial_filter (Default: 2)
@@ -53,6 +53,7 @@ class PlexusNet():
         self.MIL_mode=MIL_mode
         self.MIL_bag_size=MIL_bag_size
         self.GlobalPooling =GlobalPooling
+        self.useGated = MIL_useGated
         self.apply_RandomFourierFeatures = apply_RandomFourierFeatures
         shape_default  = (self.input_shape[0], self.input_shape[1], self.number_input_channel)
         x = layers.Input(shape=shape_default)
@@ -337,7 +338,8 @@ class PlexusNet():
         #FC: You can change here whatever you want.
         if self.get_last_conv:
             return y
-        if self.GlobalPooling==None:
+        
+        if self.GlobalPooling is None:
             y = layers.Flatten()(y)
         if self.GlobalPooling=="max":
             y = layers.GlobalMaxPooling2D()(y)
@@ -346,12 +348,208 @@ class PlexusNet():
             
         dense_shape = y.shape.as_list()[-1]
         #dense_shape = 1024
+        if self.MIL_mode:
+            y = layers.Dense(dense_shape, activation= 'selu')(y)
+            alpha = Mil_Attention(L_dim=dense_shape, output_dim=1, kernel_regularizer=l2(weight_decay), name='alpha', use_gated=self.useGated)(y)
+            x_mul = multiply([alpha, y])
+            y = Last_Sigmoid(output_dim=1, name='FC1_sigmoid')(x_mul)
+            return y
+            
         if self.apply_RandomFourierFeatures:
             y = RandomFourierFeatures(output_dim=dense_shape, scale=10.0, kernel_initializer="gaussian")
         else:
-            y = layers.Dense(dense_shape, activation= 'relu')(y)
+            y = layers.Dense(dense_shape, activation= 'selu')(y)
         y = layers.Dense(self.n_class, activation=self.final_activation)(y)
         return y
     def Save(self, filename):
         self.model.save(filename)
         print("saved...")
+        
+        
+        
+from keras.layers import Layer
+from keras import backend as K
+from keras import activations, initializers, regularizers
+
+class Mil_Attention(Layer):
+    """
+    Mil Attention Mechanism
+    This layer contains Mil Attention Mechanism
+    # Input Shape
+        2D tensor with shape: (batch_size, input_dim)
+    # Output Shape
+        2D tensor with shape: (1, units)
+    """
+
+    def __init__(self, L_dim, output_dim, kernel_initializer='glorot_uniform', kernel_regularizer=None,
+                    use_bias=True, use_gated=False, **kwargs):
+        self.L_dim = L_dim
+        self.output_dim = output_dim
+        self.use_bias = use_bias
+        self.use_gated = use_gated
+
+        self.v_init = initializers.get(kernel_initializer)
+        self.w_init = initializers.get(kernel_initializer)
+        self.u_init = initializers.get(kernel_initializer)
+
+
+        self.v_regularizer = regularizers.get(kernel_regularizer)
+        self.w_regularizer = regularizers.get(kernel_regularizer)
+        self.u_regularizer = regularizers.get(kernel_regularizer)
+
+        super(Mil_Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+
+        assert len(input_shape) == 2
+        input_dim = input_shape[1]
+
+        self.V = self.add_weight(shape=(input_dim, self.L_dim),
+                                      initializer=self.v_init,
+                                      name='v',
+                                      regularizer=self.v_regularizer,
+                                      trainable=True)
+
+
+        self.w = self.add_weight(shape=(self.L_dim, 1),
+                                    initializer=self.w_init,
+                                    name='w',
+                                    regularizer=self.w_regularizer,
+                                    trainable=True)
+
+
+        if self.use_gated:
+            self.U = self.add_weight(shape=(input_dim, self.L_dim),
+                                     initializer=self.u_init,
+                                     name='U',
+                                     regularizer=self.u_regularizer,
+                                     trainable=True)
+        else:
+            self.U = None
+
+        self.input_built = True
+
+
+    def call(self, x, mask=None):
+        n, d = x.shape
+        ori_x = x
+        # do Vhk^T
+        x = K.tanh(K.dot(x, self.V)) # (2,64)
+
+        if self.use_gated:
+            gate_x = K.sigmoid(K.dot(ori_x, self.U))
+            ac_x = x * gate_x
+        else:
+            ac_x = x
+
+        # do w^T x
+        soft_x = K.dot(ac_x, self.w)  # (2,64) * (64, 1) = (2,1)
+        alpha = K.softmax(K.transpose(soft_x)) # (2,1)
+        alpha = K.transpose(alpha)
+        return alpha
+
+    def compute_output_shape(self, input_shape):
+        shape = list(input_shape)
+        assert len(shape) == 2
+        shape[1] = self.output_dim
+        return tuple(shape)
+
+    def get_config(self):
+        config = {
+            'output_dim': self.output_dim,
+            'v_initializer': initializers.serialize(self.V.initializer),
+            'w_initializer': initializers.serialize(self.w.initializer),
+            'v_regularizer': regularizers.serialize(self.v_regularizer),
+            'w_regularizer': regularizers.serialize(self.w_regularizer),
+            'use_bias': self.use_bias
+        }
+        base_config = super(Mil_Attention, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+class Last_Sigmoid(Layer):
+    """
+    Attention Activation
+    This layer contains a FC layer which only has one neural with sigmoid actiavtion
+    and MIL pooling. The input of this layer is instance features. Then we obtain
+    instance scores via this FC layer. And use MIL pooling to aggregate instance scores
+    into bag score that is the output of Score pooling layer.
+    This layer is used in mi-Net.
+    # Arguments
+        output_dim: Positive integer, dimensionality of the output space
+        kernel_initializer: Initializer of the `kernel` weights matrix
+        bias_initializer: Initializer of the `bias` weights
+        kernel_regularizer: Regularizer function applied to the `kernel` weights matrix
+        bias_regularizer: Regularizer function applied to the `bias` weights
+        use_bias: Boolean, whether use bias or not
+        pooling_mode: A string,
+                      the mode of MIL pooling method, like 'max' (max pooling),
+                      'ave' (average pooling), 'lse' (log-sum-exp pooling)
+    # Input shape
+        2D tensor with shape: (batch_size, input_dim)
+    # Output shape
+        2D tensor with shape: (1, units)
+    """
+    def __init__(self, output_dim, kernel_initializer='glorot_uniform', bias_initializer='zeros',
+                    kernel_regularizer=None, bias_regularizer=None,
+                    use_bias=True, **kwargs):
+        self.output_dim = output_dim
+
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+
+        self.use_bias = use_bias
+        super(Last_Sigmoid, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 2
+        input_dim = input_shape[1]
+
+        self.kernel = self.add_weight(shape=(input_dim, self.output_dim),
+                                        initializer=self.kernel_initializer,
+                                        name='kernel',
+                                        regularizer=self.kernel_regularizer)
+
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.output_dim,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer)
+        else:
+            self.bias = None
+
+        self.input_built = True
+
+    def call(self, x, mask=None):
+        n, d = x.shape
+        x = K.sum(x, axis=0, keepdims=True)
+        # compute instance-level score
+        x = K.dot(x, self.kernel)
+        if self.use_bias:
+            x = K.bias_add(x, self.bias)
+
+        # sigmoid
+        out = K.sigmoid(x)
+
+
+        return out
+
+    def compute_output_shape(self, input_shape):
+        shape = list(input_shape)
+        assert len(shape) == 2
+        shape[1] = self.output_dim
+        return tuple(shape)
+
+    def get_config(self):
+        config = {
+            'output_dim': self.output_dim,
+            'kernel_initializer': initializers.serialize(self.kernel.initializer),
+            'bias_initializer': initializers.serialize(self.bias_initializer),
+            'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+            'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+            'use_bias': self.use_bias
+        }
+        base_config = super(Last_Sigmoid, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))      
