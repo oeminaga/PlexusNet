a9a51c4cc7a4d2331f43b39c566abb51dfbecd20
oeminaga <okyaz.eminaga@stanford.edu>
2020-12-22 20:12:44 -0800

    dln

--- plexusnet/architecture.py
+++ plexusnet/architecture.py
@@ -886,12 +886,13 @@ class MultiHeadSelfAttention(layers.Layer):
     def get_config(self):
         config = super().get_config().copy()
         config.update({"embed_dim": self.embed_dim,
-            "num_heads": self.num_heads,
+            "num_heads": self.num_heads
             #"projection_dim" : self.projection_dim,
-            "query_dense" : self.query_dense,
-            "key_dense" : self.key_dense,
-            "value_dense" : self.value_dense,
-            "combine_heads" : self.combine_heads})
+            #"query_dense" : self.query_dense,
+            #"key_dense" : self.key_dense,
+            #"value_dense" : self.value_dense,
+            #"combine_heads" : self.combine_heads
+            })
         return config
 class TransformerBlock(layers.Layer):
     def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1, **kwargs):
