ee70d0db6b26bb96ba4564b1a2f03e2d03c47ba4
oeminaga <okyaz.eminaga@stanford.edu>
2021-01-20 05:23:46 -0800

    updated lanewise augmentation

--- plexusnet/architecture.py
+++ plexusnet/architecture.py
@@ -32,7 +32,7 @@ Configuration={}
 Configuration["num_heads"]=4
 Configuration["number_of_transformer_blocks"]=1
 class PlexusNet():
-    def __init__(self, input_shape=(512,512), initial_filter=2, length=2, depth=7, junction=3, n_class=2, number_input_channel=3, compression_rate=0.5,final_activation="softmax", random_junctions=True, run_all_BN=True ,type_of_block="inception", run_normalization=True, run_rescale=True, filter_num_for_first_convlayer=32, kernel_size_for_first_convlayer=(5,5),stride_for_first_convlayer=2,activation_for_first_convlayer="relu", add_crop_layer=False, crop_boundary=((5,5),(5,5)), get_last_conv=False, normalize_by_factor=1.0/255.0, apply_RandomFourierFeatures=False,MIL_mode=False, MIL_CONV_mode=False, MIL_FC_percentage_of_feature=0.01, MIL_useGated=False,SCL=False,CPC=False, terms=4, predict_terms=4, code_size=256, GlobalPooling="max", RunLayerNormalizationInSCL=True, ApplyTransformer=False, number_of_transformer_blocks=1, propogate_img=False,apply_augmentation=False):
+    def __init__(self, input_shape=(512,512), initial_filter=2, length=2, depth=7, junction=3, n_class=2, number_input_channel=3, compression_rate=0.5,final_activation="softmax", random_junctions=True, run_all_BN=True ,type_of_block="inception", run_normalization=True, run_rescale=True, filter_num_for_first_convlayer=32, kernel_size_for_first_convlayer=(5,5),stride_for_first_convlayer=2,activation_for_first_convlayer="relu", add_crop_layer=False, crop_boundary=((5,5),(5,5)), get_last_conv=False, normalize_by_factor=1.0/255.0, apply_RandomFourierFeatures=False,MIL_mode=False, MIL_CONV_mode=False, MIL_FC_percentage_of_feature=0.01, MIL_useGated=False,SCL=False,CPC=False, terms=4, predict_terms=4, code_size=256, GlobalPooling="max", RunLayerNormalizationInSCL=True, ApplyTransformer=False, number_of_transformer_blocks=1, propogate_img=False,apply_augmentation=False, lanewise_augmentation=False):
         """
         Architecture hyperparameter are:
         initial_filter (Default: 2)
@@ -45,6 +45,7 @@ class PlexusNet():
         """
         self.input_shape= input_shape
         self.n_class = n_class
+        self.lanewise_augmentation = lanewise_augmentation
         self.initial_filter=initial_filter
         self.length = length
         self.depth =depth
@@ -75,6 +76,21 @@ class PlexusNet():
         self.ApplyTransformer = ApplyTransformer
         shape_default  = (self.input_shape[0], self.input_shape[1], self.number_input_channel)
         x = layers.Input(shape=shape_default)
+        if self.lanewise_augmentation:
+            self.data_augmentation = []
+            def NoAug(x):
+                return x
+            self.data_augmentation.append(keras.Sequential([
+            layers.experimental.preprocessing.RandomRotation(0.2)]))
+            
+            self.data_augmentation.append(keras.Sequential([
+            layers.experimental.preprocessing.RandomContrast(0.1)]))
+            
+            self.data_augmentation.append(NoAug)
+            
+            self.data_augmentation.append(keras.Sequential([
+            layers.experimental.preprocessing.RandomTranslation(0.2,0.2)]))
+
         if self.apply_augmentation:
             data_augmentation = keras.Sequential([
             layers.experimental.preprocessing.RandomFlip("horizontal"),
@@ -243,6 +259,9 @@ class PlexusNet():
         node = []
         x = x_input
         for i in range(depth):
+            if self.lanewise_augmentation:
+                _c = random.choice(list(range(0,4)))
+                x = self.data_augmentation[_c](x)
             x = self._conv_block(x, filter*(i+1)+2, reduction_channel_ratio=compression, kernel_regularizer=kernel_regularizer, seed=(i+counter), type_of_block=type_of_block, initial_image=initial_image)
             node.append(x)
             if self.MIL_mode:
