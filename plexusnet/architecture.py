7a945e8d354bddc054b5cf46a504bff18400a873
oeminaga <okyaz.eminaga@stanford.edu>
2021-11-17 23:05:07 -0800

    update

--- plexusnet/architecture.py
+++ plexusnet/architecture.py
@@ -271,13 +271,13 @@ class Patches(layers.Layer):
         batch_size = tf.shape(images)[0]
         patches = tf.image.extract_patches(
             images=images,
-            sizes=[1, self.patch_size, self.patch_size, 3],
-            strides=[1, self.patch_size, self.patch_size, 3],
+            sizes=[1, self.patch_size, self.patch_size,1],
+            strides=[1, self.patch_size, self.patch_size, 1],
             rates=[1, 1, 1, 1],
             padding="VALID",
         )
         patch_dims = patches.shape[-1]
-        patches = tf.reshape(patches, [batch_size,patch_dims,self.patch_size,self.patch_size, 3])
+        patches = tf.reshape(patches, [batch_size,-1,self.patch_size,self.patch_size, 3])
         return patches
 class PlexusNet():
     def __init__(self, input_shape=(512,512), number_inputs=1,initial_filter=2, length=2, depth=7, junction=3, n_class=2, number_input_channel=3, compression_rate=0.5,final_activation="softmax", random_junctions=True, run_all_BN=True ,type_of_block="inception", run_normalization=True, run_rescale=True, filter_num_for_first_convlayer=32, kernel_size_for_first_convlayer=(5,5),stride_for_first_convlayer=2,activation_for_first_convlayer="relu", add_crop_layer=False, crop_boundary=((5,5),(5,5)), get_last_conv=False, normalize_by_factor=1.0/255.0, apply_RandomFourierFeatures=False,MIL_mode=False, MIL_CONV_mode=False, MIL_FC_percentage_of_feature=0.01, MIL_useGated=False,SCL=False,CPC=False, terms=4, predict_terms=4, code_size=256, GlobalPooling="max", RunLayerNormalizationInSCL=True, ApplyTransformer=False, number_of_transformer_blocks=1, propogate_img=False,apply_augmentation=False, lanewise_augmentation=False, ApplyLayerNormalization=False, ApplyLaneForAugmentation=[0],run_255_division=True,kernel_regularizer=l2(0.000001),escape_fc1=False,fc1_activation="selu",kl_divergence_function=None, patch_layer=0):
@@ -369,10 +369,7 @@ class PlexusNet():
         else:
             x_x=x
         
-        if self.patch_layer>1:
-            x_x = Patches(self.input_shape[1]//self.patch_layer)(x_x)
         
-
         if propogate_img:
             x_a = layers.experimental.preprocessing.RandomRotation(0.1)(x_x)
             x_b = layers.experimental.preprocessing.RandomZoom (0.1)(x_x)
@@ -385,10 +382,15 @@ class PlexusNet():
         if run_normalization:
             x_y_o = utils.RotationThetaWeightLayer()([x_y_o,x_y_o])
             #rescale
+        
+
         if run_rescale:
             x_y_o = layers.Lambda(lambda x: (2* (x - K.min(x)/(K.max(x)-K.min(x)))-1))(x_y_o)
+        if self.patch_layer>1:
+            x_y_o = Patches(self.input_shape[1]//self.patch_layer)(x_y_o)
         if add_crop_layer:
             x_y_o = layers.Cropping2D(cropping=crop_boundary)(x_y_o)
+
         #Generate multiple channels from the image
         if self.patch_layer>1:
             y = self.Core(x_y_o, initial_filter = self.initial_filter, kernel_regularizer=self.kernel_regularizer, length=self.length, depth=self.depth, number_of_junctions=self.junction, compression=self.compression_rate, type_of_block=self.type_of_block)
@@ -663,7 +665,7 @@ class PlexusNet():
         nodes = []
         #Generate nodes
         if self.patch_layer>1:
-            length=self.patch_layer
+            length=self.patch_layer**2
         for i in range(length):
             if self.lanewise_augmentation:
                 if i in self.ApplyLaneForAugmentation:
