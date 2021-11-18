9b9e9e6f85128e8ecba08df856ffb3b6e5c51d35
oeminaga <okyaz.eminaga@stanford.edu>
2021-11-17 22:21:59 -0800

    Fix error

--- plexusnet/architecture.py
+++ plexusnet/architecture.py
@@ -276,8 +276,8 @@ class Patches(layers.Layer):
             rates=[1, 1, 1, 1],
             padding="VALID",
         )
-        #patch_dims = patches.shape[-1]
-        #patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size,patch_dims,self.patch_size,self.patch_size, 3])
         return patches
 class PlexusNet():
     def __init__(self, input_shape=(512,512), number_inputs=1,initial_filter=2, length=2, depth=7, junction=3, n_class=2, number_input_channel=3, compression_rate=0.5,final_activation="softmax", random_junctions=True, run_all_BN=True ,type_of_block="inception", run_normalization=True, run_rescale=True, filter_num_for_first_convlayer=32, kernel_size_for_first_convlayer=(5,5),stride_for_first_convlayer=2,activation_for_first_convlayer="relu", add_crop_layer=False, crop_boundary=((5,5),(5,5)), get_last_conv=False, normalize_by_factor=1.0/255.0, apply_RandomFourierFeatures=False,MIL_mode=False, MIL_CONV_mode=False, MIL_FC_percentage_of_feature=0.01, MIL_useGated=False,SCL=False,CPC=False, terms=4, predict_terms=4, code_size=256, GlobalPooling="max", RunLayerNormalizationInSCL=True, ApplyTransformer=False, number_of_transformer_blocks=1, propogate_img=False,apply_augmentation=False, lanewise_augmentation=False, ApplyLayerNormalization=False, ApplyLaneForAugmentation=[0],run_255_division=True,kernel_regularizer=l2(0.000001),escape_fc1=False,fc1_activation="selu",kl_divergence_function=None, patch_layer=0):
