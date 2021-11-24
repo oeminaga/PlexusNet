253d97c3404f754f16d19a57526d8ed49f7974af
oeminaga <okyaz.eminaga@stanford.edu>
2021-11-24 15:00:11 -0800

    This test

--- plexusnet/utils.py
+++ plexusnet/utils.py
@@ -53,7 +53,7 @@ class RotationThetaWeightLayerCustomWeight(Layer): # a scaled layer
         return K.cos(self.W1*90) * (-2) * K.exp(-(a**2+b**2)) + K.sin(self.W2*90) * (-2) * b * K.exp(-(a**2+b**2))
 
     def get_config(self):
-        config = super().get_config()
+        config = super(RotationThetaWeightLayerCustomWeight, self).get_config()
         config.update({
             "W1": self.W1,
             "W2": self.W2,
