ed64a368b09b0a4fc6fc6e5d90864b50d7fcb500
Okyaz Eminaga <okyaz.eminaga@stanford.edu>
2022-11-30 17:36:25 -0800

    Update utils.py

--- plexusnet/utils.py
+++ plexusnet/utils.py
@@ -138,12 +138,12 @@ class RotationThetaWeightLayer(Layer): # a scaled layer
     def build(self, input_shape):
         assert isinstance(input_shape, list)
         
-        self.W1 = self.add_weight(name='kernel_01', 
+        self.W1 = self.add_weight(#name='kernel_01', 
                                       shape=(1,),
                                       initializer='uniform',
                                       trainable=True,
                                       constraint=min_max_norm(min_value=0.0, max_value=1.0))
-        self.W2 = self.add_weight(name='kernel_02', 
+        self.W2 = self.add_weight(#name='kernel_02', 
                                       shape=(1,),
                                       initializer='uniform',
                                       trainable=True,
@@ -194,12 +194,12 @@ class JunctionWeightLayer(Layer): # a junction layer
         input_shape describes the number of the junctions.
         '''
         assert isinstance(input_shape, list)
-        self.W1 = self.add_weight(name='junction_weight_first_element', 
+        self.W1 = self.add_weight(#name='junction_weight_first_element', 
                                       shape=(1,),
                                       initializer='uniform',
                                       trainable=True,
                                       constraint=min_max_norm(min_value=0, max_value=1))
-        self.W2 = self.add_weight(name='junction_weight_second_element', 
+        self.W2 = self.add_weight(#name='junction_weight_second_element', 
                                       shape=(1,),
                                       initializer='uniform',
                                       trainable=True,
