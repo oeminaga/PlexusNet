ada41f25eb893f4edff8080781d90704f36de4bf
Okyaz Eminaga <okyaz.eminaga@gmail.com>
2023-09-05 19:10:39 -0700

    Update setup.py (#4)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ description='PlexusNet for medical image analysis',
 url='https://github.com/oeminaga/PlexusNet.git',
 author='Okyaz Eminaga',
 author_email='okyaz.eminaga@gmail.com',
-install_requires=['keras>=2.3.1', 'numpy>=1.9.1'],
+install_requires=['keras>=2.3.1', 'numpy>=1.9.1', 'tensorflow', 'tensorflow-addons', 'tensorflow-probability', 'seaborn', 'joblib', 'scikit-learn', 'opencv-python', 'pyaml'],
 license='GPL. Please cite when you use, replicate or modfiy the package.',
 packages=find_packages(),
 zip_safe=False)
